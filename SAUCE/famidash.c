e01926658a51a80bd17317da9a346b4ce65221e3
zephyrside <zephyrside@proton.me>
2023-11-03 13:29:16 -0400

    put the readme back. paths fixed in crt0.s as well

--- SAUCE/famidash.c
+++ SAUCE/famidash.c
@@ -6,6 +6,7 @@ void main(){
     load_ground(0);
     ppu_on_all();
     famistudio_music_play(song_geometry_dash_menu_theme);
+    set_scroll_y(0x100);
     while (1){
         ppu_wait_nmi();
     }
