61f6a0bab07904e52f7bf3da9d38c6a88b95947c
zephyrside <zephyrside@proton.me>
2023-11-05 11:41:45 -0500

    added nes.lib

--- SAUCE/level_decomp.h
+++ SAUCE/level_decomp.h
@@ -12,10 +12,10 @@ void load_next_column(unsigned char level, unsigned char column){
         run = level_list[level][1];
     }
     while (tmp1 < 27) { // level is 27 tiles high, so run for 27 tiles
-        columnBuffer[(tmp1)] = value; // write a value to the column buffer
+        columnBuffer[tmp1] = value; // write a value to the column buffer
 
         tmp2 = columnBuffer[tmp1];
-        collisionMap[(tmp1)+column] = tmp2;
+        collisionMap[tmp1+column] = tmp2;
         // NEVER use two pointers on the same line.
         // it will compile to 55 instructions whereas doing the above compiles to just 10
 
@@ -23,7 +23,7 @@ void load_next_column(unsigned char level, unsigned char column){
         ++tmp1; // increment column buffer write location
         if (run = 0){
             ++i; // go to the next rle index in the level data
-            value = level_list[level][(i*2)]; // set the value and run to the next tile type and length
+            value = level_list[level][i*2]; // set the value and run to the next tile type and length
             run = level_list[level][(i*2)+1];
         }
     }
