27ad732dde04d3dd13fd4dab35755e25c4a07826
ADM228 <msh.alexgoog@gmail.com>
2023-11-20 00:37:41 +0300

    New level format

--- LEVELS/levellist.h
+++ LEVELS/levellist.h
@@ -1,15 +1,30 @@
 // LEVEL ORDERING =============================================
 // 
 // NOTE: DO *NOT* PUT LEVEL DATA HERE.
-// Level order is defined here, and is interleaved with sprite
-// data. The level data pointer should come first, followed by
-// the sprite data pointer for that level. Here is an example:
+// Level order is defined here, and consists of 3 tables. The 
+// first table specifies the level data pointers, the second -
+// sprite data pointers, and the third one - interleaved ROM
+// banks for both (in order of starting level data bank, 
+// starting sprite data bank). Here is an example:
 // ============================================================
 // const unsigned char * const your_moms_house[] = {
-//    level_name_1_, level_name_1_SP,
-//    level_name_2_, level_name_2_SP,
-//    level_name_3_, level_name_3_SP,
+//    level_name_1_, 
+//    level_name_2_,
+//    level_name_3_
 //  };
+//
+// const unsigned char * const your_moms_sprites[] = {
+//    level_name_1_SP,
+//    level_name_2_SP,
+//    level_name_3_SP,
+// };
+//
+// const unsigned char your_moms_bank_account[] = {
+//    0, 3, // Level data starts in bank 0,
+//          // sprite data starts in bank 3
+//    1, 3,
+//    2, 4
+// }
 // ============================================================
 // go wild
 
@@ -18,6 +33,15 @@ const unsigned char * const level_list[] = {
     stereomadness_,
     polargeist_
     };
+
+const unsigned char * const sprite_list[] = {
+    // no sprite data for now lmfao
+};
+
+const unsigned char bank_list[] = {
+    0,
+    1
+};
 
 
 // below here is just extra dev stuff. don't uncomment. //
