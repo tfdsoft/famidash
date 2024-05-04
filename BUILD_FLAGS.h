805e88b79cca43857766bd0c914ceb6c9d95e5e0
zephyrside <zephyrside@proton.me>
2024-05-03 21:35:58 -0400

    version number on title screen (can be disabled using BUILD_FLAGS)

--- BUILD_FLAGS.h
+++ BUILD_FLAGS.h
@@ -1,8 +1,11 @@
-// set this to 0 if you are compiling a release build of the game
-#define FLAG_BETA_BUILD 1
+// set to 1 to enable the version number on the title screen
+#define FLAG_ENABLE_VER_NUM 1
+
+// set to 1 if you are compiling a beta build
+#define FLAG_BETA_BUILD 0
 
 // major version number
-#define FLAG_MAJ_VER '1'
+#define FLAG_MAJ_VER '0'
 
 // minor version number
-#define FLAG_MIN_VER '0'
+#define FLAG_MIN_VER '5'
