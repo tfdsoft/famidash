efe58ad69d0093b180a50f45e9adb8518bd39925
kandowontu <kandowontu@me.com>
2024-05-22 21:28:26 -0400

    Add explosion sprites to bankicon0D

--- SAUCE/gamestates/state_game.h
+++ SAUCE/gamestates/state_game.h
@@ -155,7 +155,7 @@ void state_game(){
 		kandoframecnt++;
         ppu_wait_nmi();
 
-	if (level == 0x0B || level == 0x0A || level == 0x09) {
+	if (level == 0x0C || level == 0x0B || level == 0x0A || level == 0x09) {
 		mmc3_set_1kb_chr_bank_0((parallax_scroll_x & 1) * 8 + 0 + 12);
 		mmc3_set_1kb_chr_bank_1((parallax_scroll_x & 1) * 8 + 1 + 12);
 		mmc3_set_1kb_chr_bank_2(parallax_scroll_x + GET_BANK(PARALLAX_CHR));
