32492eeb73a10625e138a41ef7d08aeb0fd466ae
kandowontu <kandowontu@me.com>
2024-05-27 21:17:49 -0400

    Can only START practice in pause, can continue to drop diamonds while out

--- SAUCE/gamestates/state_game.h
+++ SAUCE/gamestates/state_game.h
@@ -241,6 +241,36 @@ void state_game(){
 
 		if (pad[controllingplayer] & PAD_SELECT && pad_new[controllingplayer] & PAD_UP) currplayer_gravity ^= 0x01;
 
+				if ((pad_new[controllingplayer] & PAD_B) && PRACTICE_ENABLED && has_practice_point) {
+			// player_gravity[currplayer] ^= 0x01;			//DEBUG GRAVITY
+					if (!has_practice_point) music_play(song_practice);
+					practice_player_x[0] = player_x[0];
+					long_temp_x = high_byte(player_x[0]);
+					practice_player_x[1] = player_x[1];
+					practice_player_y[0] = player_y[0];
+					practice_player_y[1] = player_y[1];
+					practice_player_vel_x[0] = player_vel_x[0];
+					practice_player_vel_x[1] = player_vel_x[1];
+					practice_player_vel_y[0] = player_vel_y[0];
+					practice_player_vel_y[1] = player_vel_y[1];
+					practice_player_gravity[0] = player_gravity[0];
+					practice_player_gravity[1] = player_gravity[1];
+					practice_cube_rotate[0] = cube_rotate[0];
+					practice_cube_rotate[1] = cube_rotate[1];
+					practice_player_gamemode = gamemode;
+					practice_mini = mini;
+					practice_dual = dual;
+					practice_speed = speed;
+					practice_scroll_x = scroll_x;
+					practice_scroll_y = scroll_y;
+					practice_bg_color_type = lastbgcolortype;
+					practice_g_color_type = lastgcolortype;
+				//	gnd_palette_transition_timer = 0;		palete fade code
+				//	bg_palette_transition_timer = 0;		palette fade code
+
+				//	memcpy(practice_famistudio_state, famistudio_state, sizeof(practice_famistudio_state));	unneeded because of practice music
+					has_practice_point = 1;
+		}
 
 
 /* palette fade code
