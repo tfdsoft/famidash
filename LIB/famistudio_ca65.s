d6dcd43d836c11297b71db8b94ed7e53a6656a58
ADM228 <91823517+ADM228@users.noreply.github.com>
2024-06-07 04:54:41 +0300

    famistudio segment

--- LIB/famistudio_ca65.s
+++ LIB/famistudio_ca65.s
@@ -84,7 +84,7 @@
 
 .define FAMISTUDIO_CA65_ZP_SEGMENT   ZEROPAGE
 .define FAMISTUDIO_CA65_RAM_SEGMENT  BSS
-.define FAMISTUDIO_CA65_CODE_SEGMENT CODE_2
+.define FAMISTUDIO_CA65_CODE_SEGMENT SND_DRV
 
 ;======================================================================================================================
 ; 2) AUDIO EXPANSION CONFIGURATION
