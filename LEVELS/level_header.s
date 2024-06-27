1bfe6641e57462cc694c4b9fe3ac950ed543718e
Ficus <153345210+FicusmcHousePlant@users.noreply.github.com>
2024-06-26 18:29:33 -0600

    Oops forgot a file

--- LEVELS/level_header.s
+++ LEVELS/level_header.s
@@ -1,4 +1,3 @@
-
 .include "music_songlist.inc"
 
 stereomadness_song_number = song_stereo_madness
@@ -7,35 +6,17 @@ stereomadness_speed       = $00
 stereomadness_bg_color    = $12
 stereomadness_grnd_color  = $02
 
+backontrack_song_number = song_back_on_track
+backontrack_game_mode   = $00
+backontrack_speed       = $00
+backontrack_bg_color    = $14
+backontrack_grnd_color  = $14
+
 polargeist_song_number = song_polargeist
 polargeist_game_mode   = $00
 polargeist_speed       = $00
 polargeist_bg_color    = $2A
 polargeist_grnd_color  = $1A
-
-cantletgo_song_number = song_cant_let_go
-cantletgo_game_mode   = $00
-cantletgo_speed       = $00
-cantletgo_bg_color    = $14
-cantletgo_grnd_color  = $04
-
-baseafterbase_song_number = song_base_after_base
-baseafterbase_game_mode   = $00
-baseafterbase_speed       = $00
-baseafterbase_bg_color    = $11
-baseafterbase_grnd_color  = $11
-
-jumper_song_number = song_jumper
-jumper_game_mode   = $00
-jumper_speed       = $00
-jumper_bg_color    = $13
-jumper_grnd_color  = $13
-
-cycles_song_number = song_cycles
-cycles_game_mode   = $00
-cycles_speed       = $00
-cycles_bg_color    = $04
-cycles_grnd_color  = $14
 
 dryout_song_number = song_dry_out
 dryout_game_mode   = $00
@@ -43,11 +24,23 @@ dryout_speed       = $00
 dryout_bg_color    = $16
 dryout_grnd_color  = $16
 
-backontrack_song_number = song_back_on_track
-backontrack_game_mode   = $00
-backontrack_speed       = $00
-backontrack_bg_color    = $14
-backontrack_grnd_color  = $14
+baseafterbase_song_number = song_base_after_base
+baseafterbase_game_mode   = $00
+baseafterbase_speed       = $00
+baseafterbase_bg_color    = $11
+baseafterbase_grnd_color  = $11
+
+cantletgo_song_number = song_cant_let_go
+cantletgo_game_mode   = $00
+cantletgo_speed       = $00
+cantletgo_bg_color    = $14
+cantletgo_grnd_color  = $04
+
+jumper_song_number = song_jumper
+jumper_game_mode   = $00
+jumper_speed       = $00
+jumper_bg_color    = $13
+jumper_grnd_color  = $13
 
 timemachine_song_number = song_time_machine
 timemachine_game_mode   = $00
@@ -55,12 +48,29 @@ timemachine_speed       = $00
 timemachine_bg_color    = $2A
 timemachine_grnd_color  = $1A
 
+cycles_song_number = song_cycles
+cycles_game_mode   = $00
+cycles_speed       = $00
+cycles_bg_color    = $04
+cycles_grnd_color  = $14
+
 xstep_song_number = song_xstep
 xstep_game_mode   = $00
 xstep_speed       = $00
 xstep_bg_color    = $1C
 xstep_grnd_color  = $0C
 
+clutterfunk_song_number = song_clutterfunk
+clutterfunk_game_mode   = $00
+clutterfunk_speed       = $00
+clutterfunk_bg_color    = $16
+clutterfunk_grnd_color  = $06
+
+theoryofeverything_song_number = song_theory_of_everything
+theoryofeverything_game_mode   = $00
+theoryofeverything_speed       = $00
+theoryofeverything_bg_color    = $0F
+theoryofeverything_grnd_color  = $04
 
 decode_song_number = song_endgame
 decode_game_mode   = $00
@@ -86,23 +96,11 @@ test3_speed       = $00
 test3_bg_color    = $16
 test3_grnd_color  = $06
 
-test4_song_number = song_endgame
+test4_song_number = song_practice
 test4_game_mode   = $00
 test4_speed       = $00
 test4_bg_color    = $12
 test4_grnd_color  = $02
-
-clutterfunk_song_number = song_clutterfunk
-clutterfunk_game_mode   = $00
-clutterfunk_speed       = $00
-clutterfunk_bg_color    = $16
-clutterfunk_grnd_color  = $06
-
-theoryofeverything_song_number = song_theory_of_everything
-theoryofeverything_game_mode   = $00
-theoryofeverything_speed       = $00
-theoryofeverything_bg_color    = $0F
-theoryofeverything_grnd_color  = $04
 
 danger_song_number = song_menu_theme
 danger_game_mode   = $22
