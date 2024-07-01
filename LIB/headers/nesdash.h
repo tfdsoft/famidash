46ae8a7638b0516b1ab7f065958e522e482ded93
ADM228 <91823517+ADM228@users.noreply.github.com>
2024-07-01 16:07:44 +0300

    WORKING PRACTICE MODE TILES WAHOO

--- LIB/headers/nesdash.h
+++ LIB/headers/nesdash.h
@@ -189,6 +189,8 @@ void pad_poll_both();
     __A__ = MSB(word), \
     __asm__("sta %v+1, y", arr))
 
+#define uint32_inc(long) (__asm__("inc %v+0 \n bne %s", long, __LINE__), __asm__("inc %v+1 \n bne %s", long, __LINE__), __asm__("inc %v+2 \n bne %s", long, __LINE__), __asm__("inc %v+3 \n  %s:", long, __LINE__))
+
 // store a word's high and low bytes into separate places
 #define storeWordSeparately(word, low, high) \
                             (__AX__ = word, \
