ec92aa2dbd4d10881fd75a86aaa6bf66e9aba2ea
ADM228 <91823517+ADM228@users.noreply.github.com>
2024-09-27 01:00:18 +0300

    Even more capturing groups lmao

--- MUSIC/binpack.py
+++ MUSIC/binpack.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # export command: dotnet path/FamiStudio.dll famidash/MUSIC/MODULES/music_master.fms famistudio-asm-export /tmp/tmp.s -famistudio-asm-format:ca65 -export-songs:find them yourself 
 
-instsizeregex = "Info: Instruments size : .+ bytes\\."
-songsizeregex = "Info: Song '.+' size: .+ bytes\\."
+instsizeregex = "Info: Instruments size : (.+) bytes\\."
+songsizeregex = "Info: Song '(.+)' size: (.+) bytes\\."
 
 songnameregex = 'Song[^\n]+Name="([^"]+)' # Song[anything but newline]Name="[anything but "]"
 nameregex = 'Name="[^"]+"'
@@ -14,14 +14,19 @@ fs_txt_path = "/tmp/tmp.txt"
 
 if __name__ == "__main__":
     import binpacking, re, os
-    instsize = int(str(re.search(instsizeregex, cmd_output)).split()[-2])
+    instsize = re.findall(instsizeregex, cmd_output)
+    if (len(instsize) != 1):
+        LookupError("Instrument size not correctly present in the text.")
+        exit(3)
+    instsize = int(instsize[0])
     songsizestrings = re.findall(songsizeregex, cmd_output)
-    songsizes = [(int(i.split()[-2]), i[i.find("'")+1:i.rfind("'")]) for i in songsizestrings if i[i.find("'")+1:i.rfind("'")] != dpcmalignername]
-    bins = binpacking.to_constant_volume(songsizes, 8192-instsize, 0)
+    print(songsizestrings)
+    songsizes = [(i[0], int(i[1])) for i in songsizestrings if i[0] != dpcmalignername]
+    bins = binpacking.to_constant_volume(songsizes, 8192-instsize, 1)
     for i in range(len(bins)):
         print(f"== Bank {i}:")
-        print("\t- Songs: "+", ".join([j[1] for j in bins[i]]))
-        print("\t- Total approx. size:", sum([j[0] for j in bins[i]]), "bytes")
+        print("\t- Songs: "+", ".join([j[0] for j in bins[i]]))
+        print("\t- Total approx. size:", sum([j[1] for j in bins[i]]), "bytes")
 
     if os.path.exists(fs_txt_path):
         fs_txt = open(fs_txt_path)
@@ -29,18 +34,18 @@ if __name__ == "__main__":
         names = re.findall(songnameregex, "\n".join(fs_txt.readlines()))
         if len(names) == 0:
             LookupError("Amount of valid songs in the FS txt file is 0.")
-            os.exit(1)
+            exit(1)
         
         if not(dpcmalignername in names):
             LookupError("DPCM aligner not found in FS txt file.")
-            os.exit(2)
+            exit(2)
 
         dpcmidx = names.index(dpcmalignername)
         
         print("List of commands to run (adjust with your path):")
 
         for i in range(len(bins)):
-            idxs = ",".join([str(names.index(j[1])) for j in bins[i]])
+            idxs = ",".join([str(names.index(j[0])) for j in bins[i]])
             print(f"dotnet path/FamiStudio.dll famidash/MUSIC/MODULES/music_master.fms famistudio-asm-export famidash/MUSIC/EXPORTS/music_{i+1}.s -famistudio-asm-format:ca65 -famistudio-asm-generate-list -export-songs:{dpcmidx},{idxs}")
 
         print("Bank lengths table:")
