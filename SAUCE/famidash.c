ff88efdee8cea8b54c825ff38cc66d0d63f58e1d
kandowontu <kandowontu@me.com>
2024-10-05 11:31:55 -0400

    SRAM issue fixes

--- SAUCE/famidash.c
+++ SAUCE/famidash.c
@@ -161,8 +161,19 @@ void setdefaultoptions() {
 	//oneptwoplayer = 0;
 	//platformer = 0;
 	options = 0; 
-
-
+		tmp2 = 0;
+		do {
+			coin1_obtained[tmp2] = 0;
+			coin2_obtained[tmp2] = 0;
+			coin3_obtained[tmp2] = 0;
+			LEVELCOMPLETE[tmp2] = 0;
+			level_completeness_normal[tmp2] = 0;
+			level_completeness_practice[tmp2] = 0;
+		} while (++tmp2 < (0x40));
+		tmp2 = 0;
+		do {
+			achievements[tmp2] = 0;
+		} while (++tmp2 < (0x20));
 	invisible = 0;
 	color1 = 0x2A;
 	color2 = 0X2C;		
