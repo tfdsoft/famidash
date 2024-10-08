5604bf8bda579ef79c35dc41e83223871d26cf12
kandowontu <kandowontu@me.com>
2024-10-08 09:22:58 -0400

    Cursed music - mild/medium/heavy

--- SAUCE/gamestates/state_lvldone.h
+++ SAUCE/gamestates/state_lvldone.h
@@ -710,6 +710,21 @@ void colordec() {
 
 #include "defines/mainmenu_charmap.h"
 
+const unsigned char cursedtext1[]="  MILD";
+const unsigned char cursedtext2[]="MEDIUM";
+const unsigned char cursedtext3[]=" HEAVY";
+
+const unsigned char* const cursedtexts[] = {
+	0, cursedtext1, cursedtext2, cursedtext3
+};
+
+const unsigned char cursedtext_size[] = {
+	0,
+	sizeof(cursedtext1) - 1,
+	sizeof(cursedtext2) - 1,
+	sizeof(cursedtext3) - 1
+};
+
 const unsigned char gameboytext1[]="  GREY";
 const unsigned char gameboytext2[]="   RED";
 const unsigned char gameboytext3[]=" GREEN";
@@ -809,8 +824,12 @@ void funsettings() {
 		if (cam_seesaw) 	one_vram_buffer('g', NTADR_A(26, 17));	// believe it or not, 
 		else 	one_vram_buffer('f', NTADR_A(26, 17));	// this is auto optimized by cc65
 		
-		if (cursedmusic) 	one_vram_buffer('g', NTADR_A(26, 21));	// believe it or not, 
-		else 	one_vram_buffer('f', NTADR_A(26, 21));	// this is auto optimized by cc65
+		__A__ = idx16_load_hi_NOC(cursedtexts, cursedmusic);
+		if (__A__) { draw_padded_text(cursedtexts[cursedmusic & 0x7F], cursedtext_size[cursedmusic], 8, NTADR_A(19, 21)); 	one_vram_buffer('g', NTADR_A(26, 19));	}// believe it or not, 
+		else { one_vram_buffer_horz_repeat('$', 8, NTADR_A(19, 21)); one_vram_buffer('f', NTADR_A(26, 21)); }
+
+//		if (cursedmusic) 	one_vram_buffer('g', NTADR_A(26, 21));	// believe it or not, 
+//		else 	one_vram_buffer('f', NTADR_A(26, 21));	// this is auto optimized by cc65
 		
 		__A__ = idx16_load_hi_NOC(gameboytexts, gameboy_mode);
 		if (__A__) { draw_padded_text(gameboytexts[gameboy_mode & 0x7F], gameboy_text_size[gameboy_mode], 8, NTADR_A(19, 19)); 	one_vram_buffer('g', NTADR_A(26, 19));	}// believe it or not, 
@@ -850,12 +869,6 @@ void funsettings() {
 		}
 			
 		if (joypad1.press_b) {
-			// one_vram_buffer(' ', NTADR_A(6, 8));
-			// one_vram_buffer(' ', NTADR_A(6, 12));
-			// one_vram_buffer(' ', NTADR_A(6, 16));
-			//one_vram_buffer(' ', NTADR_A(4, 8));
-			//one_vram_buffer(' ', NTADR_A(4, 12));
-			//one_vram_buffer(' ', NTADR_A(4, 16));
 			gameState = last_gameState;
 			return;
 		}
@@ -885,7 +898,7 @@ void set_fun_settings() {
 		case 0x04: invisblocks ^= 1; break;
 		case 0x05: cam_seesaw = (cam_seesaw > 0 ? 0 : 1); break;
 		case 0x06: gameboy_mode = (gameboy_mode == 8 ? 0 : gameboy_mode + 1); break;
-		case 0x07: cursedmusic ^= 1; break;
+		case 0x07: cursedmusic += (cursedmusic == 3 ? -3 : 1); break;
 	};
 }	
 
@@ -917,142 +930,7 @@ void code_checker() {
 
 
 
-#include "defines/mainmenu_charmap.h"
 
-void settings() {
-	settingvalue = 0; 
-	pal_fade_to_withmusic(4,0);
-	mmc3_disable_irq();
-	ppu_off();
-	pal_bg(paletteSettings);
-	vram_adr(NAMETABLE_A);
-	vram_unrle(settingscreen);   	
-	mmc3_set_2kb_chr_bank_0(0xFF);
-	mmc3_set_2kb_chr_bank_1(MOUSEBANK);
-	ppu_on_all();
-	one_vram_buffer('c', NTADR_A(4, 7));	// settingvalue is set to 0 beforehand
-	pal_fade_to_withmusic(0,4);
-	while (1) {
-		ppu_wait_nmi();
-		music_update();
-		oam_clear();
-		mouse_and_cursor();
-		 // read the first controller
-		
-		if (twoplayer) one_vram_buffer('g', NTADR_A(26, 7));
-		else one_vram_buffer('f', NTADR_A(26, 7));
-
-		if (options & oneptwoplayer) one_vram_buffer('g', NTADR_A(26, 9));
-		else one_vram_buffer('f', NTADR_A(26, 9));
-
-		if (options & sfxoff) one_vram_buffer('f', NTADR_A(26, 11));
-		else one_vram_buffer('g', NTADR_A(26, 11));
-
-		if (options & musicoff) one_vram_buffer('f', NTADR_A(26, 13));
-		else one_vram_buffer('g', NTADR_A(26, 13));
-
-		if (options & jumpsound) one_vram_buffer('g', NTADR_A(26, 15));
-		else one_vram_buffer('f', NTADR_A(26, 15));
-
-		if (viseffects) one_vram_buffer('g', NTADR_A(26, 17));
-		else one_vram_buffer('f', NTADR_A(26, 17));
-
-		if (trails == 1) one_vram_buffer('g', NTADR_A(26, 19));
-		else if (trails == 2) one_vram_buffer('*', NTADR_A(26, 19));
-		else one_vram_buffer('f', NTADR_A(26, 19));
-
-		tmp1 = settingvalue;
-
-		if (mouse.left_press) {
-			if (mouse.x >= 0x2D && mouse.x <= 0xDD) {
-				if (mouse.y >= 0x34 && mouse.y <= 0x3C) {
-					settingvalue = 0; set_settings();
-				}
-				else if (mouse.y >= 0x44 && mouse.y <= 0x4C) {
-					settingvalue = 1; set_settings();
-				}
-				else if (mouse.y >= 0x54 && mouse.y <= 0x5C) {
-					settingvalue = 2; set_settings();
-				}
-				else if (mouse.y >= 0x64 && mouse.y <= 0x6C) {
-					settingvalue = 3; set_settings();
-				}
-				else if (mouse.y >= 0x74 && mouse.y <= 0x7C) {
-					settingvalue = 4; set_settings();
-				}
-				else if (mouse.y >= 0x84 && mouse.y <= 0x8C) {
-					settingvalue = 5; set_settings();
-				}
-				else if (mouse.y >= 0x94 && mouse.y <= 0x9C) {
-					settingvalue = 6; set_settings();
-				}
-
-			}
-			if ((mouse.x >= 0x1D && mouse.x <= 0xDD) && (mouse.y >= 0xBC && mouse.y <= 0xC4)) {		
-				return;
-			}
-
-		}	
-
-		if (joypad1.press & (PAD_RIGHT | PAD_DOWN)) {
-			if (settingvalue == 7) { settingvalue = 0;  }
-			else { settingvalue++;   }
-		}
-
-		if (joypad1.press & (PAD_LEFT | PAD_UP)) {
-			if (settingvalue == 0) { settingvalue = 7;  }
-			else { settingvalue--;   }
-		}
-
-		if (tmp1 != settingvalue) {
-			// NTADR_A = (NAMETABLE_A|(((y)<<5)|(x)))
-			// (tmp1 * 2) << 5 = tmp1<<6 = (tmp1<<8)>>2
-			one_vram_buffer(' ', NTADR_A(4, 7)+((tmp1<<8)>>2));
-			one_vram_buffer('c', NTADR_A(4, 7)+((settingvalue<<8)>>2));
-		}
-		
-		if (joypad1.press & (PAD_A | PAD_START)) {
-			set_settings();
-		}
-		if (options & platformer) {
-			twoplayer = 0;
-			one_vram_buffer('X',NTADR_A(26,7));
-		}
-		if (twoplayer) options &= platformer^0xff;		
-
-		if (joypad1.press_b) {
-			return;
-		}
-		kandoframecnt++;
-		if (kandoframecnt & 1 && mouse_timer) mouse_timer--;	
-				
-	}
-}
-
-void set_settings() {
-	switch (settingvalue) {
-		case 0: // oneptwoplayer
-			twoplayer ^= 1; break;
-		case 1: // oneptwoplayer
-			options ^= oneptwoplayer; break;
-		case 2: // sfxoff
-			options ^= sfxoff; break;
-		case 3: // musicoff
-			options ^= musicoff; if (options & musicoff) { famistudio_music_stop(); music_update(); } else { music_play(song_menu_theme); } break;
-		case 4: // jumpsound
-			options ^= jumpsound; break;
-		case 5:
-			viseffects ^= 1; break;
-		case 6:
-			trails = trails == 2 ? 0 : trails + 1; break;					
-		case 7:
-			if (joypad1.a && joypad1.press_start) {
-				setdefaultoptions();
-				__asm__("JMP ($FFFC)");	// restart the game lmao	
-			}
-			break;
-	}
-}	
 
 
 const unsigned char* const leveltexts[] = {
