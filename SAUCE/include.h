ca42c78a32c01de9987d1494b7d3df3c0f24144f
kandowontu <kandowontu@me.com>
2024-10-20 21:36:55 -0400

    my brother in christ wtf is this shit taking up space in xcd_bank_03

--- SAUCE/include.h
+++ SAUCE/include.h
@@ -102,7 +102,7 @@
 #include "gamestates/state_menu.h"
 #include "gamestates/state_game.h"
 
-#include "gamestates/state_sorrynothing.h"
+//#include "gamestates/state_sorrynothing.h"
 
 #include "gamestates/state_lvldone.h"
 #include "gamestates/state_demo.h"
