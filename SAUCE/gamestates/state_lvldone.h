03279071a8c89c897f75d972d8cc988abe134de5
kandowontu <kandowontu@me.com>
2024-10-20 21:35:31 -0400

    A OR start to exit/restart level

--- SAUCE/gamestates/state_lvldone.h
+++ SAUCE/gamestates/state_lvldone.h
@@ -363,7 +363,7 @@ void state_lvldone() {
 
 			if (joypad1.press_left) { menuselection ^= 1; lvl_done_update(); }
 			if (joypad1.press_right) { menuselection ^= 1; lvl_done_update(); }
-			if (joypad1.press_start){
+			if (joypad1.press_start || joypad1.press_a){
 				if (menuselection) {
 					
 					sfx_play(sfx_exit_level, 0);
