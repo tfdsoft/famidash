309fa7692b5ca91c71b985588d6c3b076e9d7ba8
kandowontu <kandowontu@me.com>
2024-10-21 21:32:00 -0400

    level testing

--- LEVELS/all_level_table.s
+++ LEVELS/all_level_table.s
@@ -165,7 +165,7 @@ _level_lengths_lo:
   .byte .lobyte($000080)		; test
   .byte .lobyte($00006E)		; test2
   .byte .lobyte($000069)		; test3
-  .byte .lobyte($000019)		; test4
+  .byte .lobyte($000069)		; test4
   .byte .lobyte($000006)		; danger
 
 .if MID_LEVEL_LENGTHS_ENABLED
@@ -190,7 +190,7 @@ _level_lengths_md:
   .byte .hibyte($000080)		; test
   .byte .hibyte($00006E)		; test2
   .byte .hibyte($000069)		; test3
-  .byte .hibyte($000019)		; test4
+  .byte .hibyte($000069)		; test4
   .byte .hibyte($000006)		; danger
 
 .if HIGH_LEVEL_LENGTHS_ENABLED
@@ -215,7 +215,7 @@ _level_lengths_hi:
   .byte .bankbyte($000080)		; test
   .byte .bankbyte($00006E)		; test2
   .byte .bankbyte($000069)		; test3
-  .byte .bankbyte($000019)		; test4
+  .byte .bankbyte($000069)		; test4
   .byte .bankbyte($000006)		; danger
 
 .endif
--- LEVELS/level_header.s
+++ LEVELS/level_header.s
@@ -111,8 +111,8 @@ test3_grnd_color  = $06
 test4_song_number = song_practice
 test4_game_mode   = $00
 test4_speed       = $00
-test4_bg_color    = $12
-test4_grnd_color  = $02
+test4_bg_color    = $0F
+test4_grnd_color  = $0F
 
 danger_song_number = song_menu_theme
 danger_game_mode   = $22
