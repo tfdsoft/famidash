cf3be7845ad9354ea7af44765e9dd4c9112f0bc1
kandowontu <kandowontu@me.com>
2024-10-23 09:30:26 -0400

    Fix all stars/coins not calculating right in custom screen

--- SAUCE/gamestates/state_menu.h
+++ SAUCE/gamestates/state_menu.h
@@ -439,7 +439,7 @@ void customize_screen() {
 
 	TOTALCOINS = 0;
 	TOTALSTARS = 0;
-
+	tmp2 = 0;
 	do {
 		// TOTALCOINS = TOTALCOINS + coin1_obtained[tmp2] + coin2_obtained[tmp2] + coin3_obtained[tmp2];
 		__A__ = tmp2; __asm__("tay");
