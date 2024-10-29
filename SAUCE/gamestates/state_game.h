118746c5a27973f6786e39d82ec05c64ecf6b7e5
kandowontu <kandowontu@me.com>
2024-10-29 12:26:44 -0400

    Move wave to same mini wave bank

--- SAUCE/gamestates/state_game.h
+++ SAUCE/gamestates/state_game.h
@@ -527,7 +527,7 @@ void set_player_banks() {
 		}
 		
 		if (gamemode == 8) mmc3_set_2kb_chr_bank_0(NINJABANK);
-		else if ((currplayer_mini && gamemode != 0) || (gamemode == 7)) mmc3_set_2kb_chr_bank_0(iconbank2);
+		else if ((currplayer_mini && gamemode != 0) || (gamemode == 7) || (gamemode == 6)) mmc3_set_2kb_chr_bank_0(iconbank2);
 		else if (gamemode == 0 || gamemode == 1 || gamemode == 3) mmc3_set_2kb_chr_bank_0(iconbank3);
 		else mmc3_set_2kb_chr_bank_0(iconbank1);
 
