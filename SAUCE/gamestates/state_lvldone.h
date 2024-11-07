68526a6490d355853834c18c4e5164bcd4583f2c
kandowontu <kandowontu@me.com>
2024-11-07 13:17:03 -0500

    eon crash b gone

--- SAUCE/gamestates/state_lvldone.h
+++ SAUCE/gamestates/state_lvldone.h
@@ -433,6 +433,7 @@ const unsigned char TEXT_xlevel2textE[]="DECODE";
 const unsigned char TEXT_2textpractice[]="PRACTICE";
 const unsigned char TEXT_2textmenu[]="MENU";
 const unsigned char TEXT_2texteveryend[]="EVERY$END";
+const unsigned char TEXT_2texteon[]="EON";
 
 const unsigned char TEXT_sfxtext0[]="DEATH";
 const unsigned char TEXT_sfxtext1[]="CLICK";
@@ -466,11 +467,11 @@ const unsigned char sfxtexts_size[] = {
 
 
 const unsigned char* const xbgmtexts1[] = {
-	0, TEXT_xlevel1text1, TEXT_xlevel1text2, 0, 0, TEXT_xlevel1text5, 0, 0, 0, 0, 0, 0, TEXT_xlevel1textC, TEXT_xlevel1textD, 0, 0
+	0, TEXT_xlevel1text1, TEXT_xlevel1text2, 0, 0, TEXT_xlevel1text5, 0, 0, 0, 0, 0, 0, TEXT_xlevel1textC, TEXT_xlevel1textD, 0, 0, 0
 };
 
 const unsigned char* const xbgmtexts2[] = {
-	TEXT_2textmenu, TEXT_xlevel2text1, TEXT_xlevel2text2, TEXT_xlevel2text3, TEXT_xlevel2text4, TEXT_xlevel2text5, TEXT_xlevel2text6, TEXT_xlevel2text7, TEXT_xlevel2text8, TEXT_xlevel2text9, TEXT_xlevel2textA, TEXT_xlevel2textB, TEXT_xlevel2textC, TEXT_xlevel2textD, TEXT_xlevel2textE, TEXT_2textpractice
+	TEXT_2textmenu, TEXT_xlevel2text1, TEXT_xlevel2text2, TEXT_xlevel2text3, TEXT_xlevel2text4, TEXT_xlevel2text5, TEXT_xlevel2text6, TEXT_xlevel2text7, TEXT_xlevel2text8, TEXT_xlevel2text9, TEXT_xlevel2textA, TEXT_xlevel2textB, TEXT_xlevel2textC, TEXT_xlevel2textD, TEXT_xlevel2textE, TEXT_2textpractice, TEXT_2texteon
 };
 
 const unsigned char xbgmtext2_size[] = {
@@ -490,6 +491,7 @@ const unsigned char xbgmtext2_size[] = {
 	sizeof(TEXT_xlevel2textD) - 1,	
 	sizeof(TEXT_xlevel2textE) - 1,
 	sizeof(TEXT_2textpractice) - 1,
+	sizeof(TEXT_2texteon) - 1,
 };
 const unsigned char xbgmtext1_size[] = {
 	0,
@@ -506,6 +508,7 @@ const unsigned char xbgmtext1_size[] = {
 	0,
 	sizeof(TEXT_xlevel1textC) - 1,	
 	sizeof(TEXT_xlevel1textD) - 1,	
+	0,
 	0,
 	0,
 };
@@ -528,6 +531,7 @@ const uint8_t xbgm_lookup_table2[] = {
 	song_electroman_adventures, 
 	song_custom_endgame, 
 	song_practice,
+	song_eon,
 };
 
 
