1c7b759cffcbc20cac6269e0b9a036e072f9965d
kandowontu <kandowontu@me.com>
2024-11-12 09:48:09 -0500

    Initial clubstep test -> test4

--- LEVELS/all_level_table.s
+++ LEVELS/all_level_table.s
@@ -193,7 +193,7 @@ _level_lengths_lo:
   .byte .lobyte($000080)		; test
   .byte .lobyte($00006E)		; test2
   .byte .lobyte($000069)		; test3
-  .byte .lobyte($000080)		; test4
+  .byte .lobyte($000091)		; test4
   .byte .lobyte($000006)		; danger
 
 .if MID_LEVEL_LENGTHS_ENABLED
@@ -222,7 +222,7 @@ _level_lengths_md:
   .byte .hibyte($000080)		; test
   .byte .hibyte($00006E)		; test2
   .byte .hibyte($000069)		; test3
-  .byte .hibyte($000080)		; test4
+  .byte .hibyte($000091)		; test4
   .byte .hibyte($000006)		; danger
 
 .if HIGH_LEVEL_LENGTHS_ENABLED
@@ -251,7 +251,7 @@ _level_lengths_hi:
   .byte .bankbyte($000080)		; test
   .byte .bankbyte($00006E)		; test2
   .byte .bankbyte($000069)		; test3
-  .byte .bankbyte($000080)		; test4
+  .byte .bankbyte($000091)		; test4
   .byte .bankbyte($000006)		; danger
 
 .endif
