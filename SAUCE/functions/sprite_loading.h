174b77a55d2d94e98423bea669d056a8a2a02fd7
kandowontu <kandowontu@me.com>
2024-12-07 23:33:32 -0500

    Revert "fix 2p mode"
    This reverts commit 00f1ff7c3901615036ee2045725c08df71b9713b.

--- SAUCE/functions/sprite_loading.h
+++ SAUCE/functions/sprite_loading.h
@@ -607,7 +607,7 @@ void sprite_collide_lookup() {
 		case DUAL_PORTAL:
 	//		if (!activesprites_activated[index]) {
 				dual = 1;
-				if (!twoplayer) target_scroll_y = (uint16SepArrLoad(activesprites_y, index) - 0x10);
+				target_scroll_y = (uint16SepArrLoad(activesprites_y, index) - 0x10);
 				if (twoplayer) { player_gravity[1] = player_gravity[0] ^ 1;  }
 				else { player_x[1] = player_x[0]; player_y[1] = currplayer_y; player_gravity[1] = !currplayer_gravity; player_vel_y[1] = -currplayer_vel_y; mini[1] = mini[0]; }
 	//			activesprites_type[index] = 0xFF;
@@ -622,7 +622,7 @@ void sprite_collide_lookup() {
 				longmode = 0;
 				bigboi = 0;
 	//			activesprites_type[index] = 0xFF;
-				if (!twoplayer) exitPortalTimer = 5;
+				exitPortalTimer = 5;
 	//		}
 			return;
 		case TELEPORT_PORTAL_EXIT:
@@ -658,7 +658,7 @@ void sprite_collide_lookup() {
 		case UFO_MODE:
 			settrailstuff();
 		case BALL_MODE:
-			if (!dual || twoplayer) target_scroll_y = (uint16SepArrLoad(activesprites_y, index) - 0x10);
+			if (!dual) target_scroll_y = (uint16SepArrLoad(activesprites_y, index) - 0x10);
 			if (gamemode == GAMEMODE_WAVE) currplayer_vel_y /= 2;
 			if (currplayer_vel_y > 0x350) currplayer_vel_y = 0x350;
 			if (currplayer_vel_y < -0x350) currplayer_vel_y = -0x350;
@@ -688,20 +688,20 @@ void sprite_collide_lookup() {
 			if (gamemode == GAMEMODE_WAVE) currplayer_vel_y = 0;
 			gamemode = GAMEMODE_SPIDER;
 			retrofireballclear();		
-			if (!dual || twoplayer) target_scroll_y = (uint16SepArrLoad(activesprites_y, index) - 0x10);		
+			if (!dual) target_scroll_y = (uint16SepArrLoad(activesprites_y, index) - 0x10);		
 			return;
 		case WAVE_MODE:
 			settrailstuff();		
 			gamemode = GAMEMODE_WAVE;
 			retrofireballclear();		
-			if (!dual || twoplayer) target_scroll_y = (uint16SepArrLoad(activesprites_y, index) - 0x10);		
+			if (!dual) target_scroll_y = (uint16SepArrLoad(activesprites_y, index) - 0x10);		
 			return;
 		case SWING_MODE:
 			settrailstuff();
 			if (gamemode == GAMEMODE_WAVE) currplayer_vel_y = 0;
 			gamemode = GAMEMODE_SWING;
 			retrofireballclear();		
-			if (!dual || twoplayer) target_scroll_y = (uint16SepArrLoad(activesprites_y, index) - 0x10);	
+			if (!dual) target_scroll_y = (uint16SepArrLoad(activesprites_y, index) - 0x10);	
 			return;
 #ifdef FLAG_KANDO_FUN_STUFF
 		case NINJA_MODE:
@@ -709,7 +709,7 @@ void sprite_collide_lookup() {
 			gamemode = GAMEMODE_NINJA;
 			return;
 		case RANDOM_MODE_PORTAL:
-			if (!dual || twoplayer) target_scroll_y = (uint16SepArrLoad(activesprites_y, index) - 0x10);
+			if (!dual) target_scroll_y = (uint16SepArrLoad(activesprites_y, index) - 0x10);
 			if (gamemode == GAMEMODE_WAVE) currplayer_vel_y = 0;		
 			gamemode = newrand() & 7;
 			idx8_inc(activesprites_activated, index);
