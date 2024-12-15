639b7dd8bc18056ee3dd0b65f46df80a90af084b
ADM228 <91823517+ADM228@users.noreply.github.com>
2024-12-15 14:44:58 +0300

    rename problematic to not be capitalized

--- LEVELS/export_levels.sh
+++ LEVELS/export_levels.sh
@@ -2,40 +2,7 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 n=0
-all_levels="
-            stereomadness\
-            backontrack\
-            polargeist\
-            dryout\
-            baseafterbase\
-            cantletgo\
-            jumper\
-            timemachine\
-            cycles\
-            xstep\
-            clutterfunk\
-            theoryofeverything\
-            electroman\
-	    clubstep\
-	    hexagonforce\
-	    toe2\
-	    thechallenge\
-	    retray\
-	    sonar\
-	    kappaclysm\
-	    revolution\
-            thelightningroad\
-	    nightmare\
-	    demonpark\
-	    deathmoon\
-	    decode\
-	    problematic\
-	    firetemple\
-            eon\
-	    pgclubstep\
-	    luckydraw\
-            test4\
-            "
+all_levels="stereomadness backontrack polargeist dryout baseafterbase cantletgo jumper timemachine cycles xstep clutterfunk theoryofeverything electroman clubstep hexagonforce toe2 retray sonar thechallenge kappaclysm revolution thelightningroad nightmare demonpark deathmoon decode problematic firetemple eon pgclubstep luckydraw test4 "
 
 echo Exporting everything ${all_levels}
 python3 ${SCRIPT_DIR}/export_levels.py --folder "${SCRIPT_DIR}/LEVEL DATA" ${all_levels}
