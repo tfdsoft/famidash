4a81703592f46ba22ccb932b0ce045f28bced2ce
kandowontu <kandowontu@me.com>
2025-02-03 23:05:54 -0500

    update build name

--- 1build.bat
+++ 1build.bat
@@ -1,5 +1,5 @@
 del /Q TMP\*.*
 bin\winmake.exe
-copy .\build\famidash.nes .\famidash.nes
-copy .\build\famidash.dbg .\famidash.dbg
+copy .\build\famidash.nes .\famidash vs.nes
+copy .\build\famidash.dbg .\famidash vs.dbg
 pause
--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 bin\winmake.exe
-copy .\build\famidash.nes .\famidash.nes
-copy .\build\famidash.dbg .\famidash.dbg
+copy .\build\famidash.nes .\famidash vs.nes
+copy .\build\famidash.dbg .\famidash vs.dbg
 pause
