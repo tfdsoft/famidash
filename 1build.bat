65f7ab9c95f73b9bd87d662fbf92114f85a0e9cc
kandowontu <kandowontu@me.com>
2025-02-03 23:05:22 -0500

    Make it build as a new rom name

--- 1build.bat
+++ 1build.bat
@@ -1,5 +1,5 @@
 del /Q TMP\*.*
 bin\winmake.exe
-copy .\build\famidash.nes .\famidash.nes
-copy .\build\famidash.dbg .\famidash.dbg
+copy .\build\famidash.nes .\Famidash B Sides.nes
+copy .\build\famidash.dbg .\Famidash B Sides.dbg
 pause
