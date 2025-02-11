9e4f5ec804b8ec2528aae6501e8527602290030e
ADM228 <91823517+ADM228@users.noreply.github.com>
2025-02-11 18:17:02 +0300

    Fix blue pads, mark more TODOs

--- SAUCE/gamemodes/gamemode_cube.h
+++ SAUCE/gamemodes/gamemode_cube.h
@@ -81,6 +81,7 @@ void cube_movement(){
 
 					if (make_cube_jump_higher) {
 						if ((currplayer_slope_type & SLOPE_DEGREES_MASK) != SLOPE_22DEG) {
+							// TODO
 							currplayer_vel_y += (currplayer_gravity ? 0x80 : -0x80);
 						}
 						make_cube_jump_higher = 0;
@@ -95,6 +96,7 @@ void cube_movement(){
 
 				if (make_cube_jump_higher) {
 					if ((currplayer_slope_type & SLOPE_DEGREES_MASK) != SLOPE_22DEG) {
+						// TODO
 						currplayer_vel_y += (currplayer_gravity ? 0x80 : -0x80);
 					}
 					make_cube_jump_higher = 0;
