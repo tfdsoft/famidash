06cbe4629562e5f168188a0d51baac45d8529a28
ADM228 <91823517+ADM228@users.noreply.github.com>
2025-02-11 18:17:02 +0300

    Fix blue pads, mark more TODOs

--- SAUCE/functions/sprite_loading.h
+++ SAUCE/functions/sprite_loading.h
@@ -1022,10 +1022,9 @@ void sprite_collide_lookup() {
 		clear_slope_stuff();
 		if (!currplayer_gravity) { 
 			settrailstuff();
-			// INTENTIONALLY do this before flipping gravity
-			currplayer_vel_y = PAD_HEIGHT_BLUE(currplayer_table_idx);
 			currplayer_gravity = GRAVITY_UP;				//flip gravity
 			update_currplayer_table_idx();
+			currplayer_vel_y = PAD_HEIGHT_BLUE(currplayer_table_idx);
 			//invincible_counter = 3;
 		}
 		idx8_inc(activesprites_activated, index);
@@ -1035,10 +1034,9 @@ void sprite_collide_lookup() {
 		clear_slope_stuff();
 		if (currplayer_gravity) { 	
 			settrailstuff();
-			// INTENTIONALLY do this before flipping gravity
-			currplayer_vel_y = PAD_HEIGHT_BLUE(currplayer_table_idx);
 			currplayer_gravity = GRAVITY_DOWN;				//flip gravity
 			update_currplayer_table_idx();
+			currplayer_vel_y = PAD_HEIGHT_BLUE(currplayer_table_idx);
 			//invincible_counter = 3;				
 		}
 		idx8_inc(activesprites_activated, index);	
@@ -1232,6 +1230,7 @@ void retrofireballclear() {
 void dual_cap_check() {
 	if (dual && !twoplayer) {
 		if (currplayer == 0) {
+			// TODO
 			if (player_vel_y[1] > 0x250) player_vel_y[1] = 0x250;
 			if (player_vel_y[1] < -0x250) player_vel_y[1] = -0x250;
 		}
