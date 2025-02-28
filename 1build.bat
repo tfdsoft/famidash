1b47fd6c35879b558d1487cc7954bdf083d015ee
xXFamidash_Fan69Xx <124401453+xXFamidashFan69Xx@users.noreply.github.com>
2025-02-28 15:08:20 -0300

    preparation
    do stuff

--- 1build.bat
+++ 1build.bat
@@ -1,5 +1,5 @@
 del /Q TMP\*.*
 bin\winmake.exe
-copy .\build\famidash.nes ".\Famidash B Sides.nes"
-copy .\build\famidash.dbg ".\Famidash B Sides.dbg"
+copy .\build\famidash.nes ".\Famidash Bananas branch.nes"
+copy .\build\famidash.dbg ".\Famidash Bananas branch.dbg"
 pause
