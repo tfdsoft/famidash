e72f2a1982010abf7cdfcaf9c9e485af386f9a0f
kandowontu <95717282+kandowontu@users.noreply.github.com>
2025-03-08 23:10:58 -0500

    d block fix?

--- SAUCE/gamestates/state_game.h
+++ SAUCE/gamestates/state_game.h
@@ -506,6 +506,7 @@ void state_game(){
 			last_slope_type[0] = currplayer_last_slope_type;
 		}
 
+		dblocked[currplayer] = 0;
 		
 		if (dual) { 
 			currplayer = 1;					//take focus
@@ -548,6 +549,8 @@ void state_game(){
 			processXMovement = 0;
 			runthecolls();
 			processXMovement = 1;
+			
+			dblocked[currplayer] = 0;
 
 			currplayer = 0;					//give back focus
 
@@ -578,7 +581,6 @@ void state_game(){
 				currplayer_last_slope_type = last_slope_type[0];
 			}
 		}
-
 	}
    //     check_spr_objects();
 
