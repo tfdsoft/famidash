46032b6e4759a48f373c97bf684f1f9c2a7dc7c8
ADM228 <91823517+ADM228@users.noreply.github.com>
2025-03-10 13:39:49 +0300

    Disable "level restarting" when level is beaten
    (cherry picked from commit 05f8da19bcfa2a50aac1d43848d7e30175a1b3b3)

--- SAUCE/gamestates/state_lvldone.h
+++ SAUCE/gamestates/state_lvldone.h
@@ -363,8 +363,11 @@ void state_lvldone() {
 				}
 			}
 
+			#if !__VS_SYSTEM	// Disable level restarting on VS system
 			if (joypad1.press_left) { menuselection ^= 1; lvl_done_update(); }
 			if (joypad1.press_right) { menuselection ^= 1; lvl_done_update(); }
+			#endif
+			
 			if (joypad1.press_start || joypad1.press_a){
 				if (menuselection) {
 					
