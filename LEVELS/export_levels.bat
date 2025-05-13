40970a0532d8af5c1f71792caa3f930c188a78bb
ADM228 <91823517+ADM228@users.noreply.github.com>
2025-05-13 16:55:27 +0300

    Fix bat file argument passing

--- LEVELS/export_levels.bat
+++ LEVELS/export_levels.bat
@@ -4,7 +4,7 @@ setlocal EnableDelayedExpansion
 cd "%~dp0"
 
 echo Exporting everything specified in metadata.json5
-python export_wrapper.py "%*"
+python export_wrapper.py %*
 
 echo Export finished.
 pause
