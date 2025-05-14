caeded90b094bb4dd3fac5be9375d9551d18732c
kandowontu <95717282+kandowontu@users.noreply.github.com>
2025-05-13 22:33:05 -0400

    Fix VS rom not opening

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ copy .\build\main\famidash.nes .\famidash.nes
 copy .\build\main\famidash.dbg .\famidash.dbg
 
 copy .\build\vs-sys\famidash.nes ".\famidash vs.nes"
-copy .\build\vs-sys\famidash.dbg ".\famidash vs.nes"
+copy .\build\vs-sys\famidash.dbg ".\famidash vs.dbg"
 
 copy .\build\b-sides\famidash.nes ".\Famidash B Sides.nes"
 copy .\build\b-sides\famidash.dbg ".\Famidash B Sides.dbg"
