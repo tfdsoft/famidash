9b002fbda3fe534132776c12b67cb9f16a919f3a
ADM228 <91823517+ADM228@users.noreply.github.com>
2025-05-22 00:07:13 +0300

    Move nametable files, get palette defs out of famidash.h

--- SAUCE/include_menus.h
+++ SAUCE/include_menus.h
@@ -1,7 +1,7 @@
 
 CODE_BANK_PUSH("XCD_BANK_03")
 
-#include "defines/menunametable.h"
+#include "defines/nametable/menunametable_XCD03.c"
 
 #include "menustates/common_xcd_bank_03.c"
 
@@ -17,7 +17,7 @@ CODE_BANK("XCD_BANK_05")
 
 CODE_BANK("XCD_BANK_06")
 
-#include "defines/menunametable2.h"
+#include "defines/nametable/menunametable_XCD06.c"
 
 #if LEVELSET != 'A'
 #include "menustates/playmain.c"
@@ -30,7 +30,8 @@ CODE_BANK("XCD_BANK_06")
 
 CODE_BANK("XCD_BANK_02")
 
-#include "defines/menunametable3.h"
+#include "defines/nametable/menunametable_XCD02.c"
+#include "defines/palette/palettes_XCD02.c"
 
 #include "menustates/instructions.c"
 #include "menustates/credits.c"
