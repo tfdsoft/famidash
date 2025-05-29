d7085c2853218a2c867710984a921362887c9110
kandowontu <95717282+kandowontu@users.noreply.github.com>
2025-05-28 21:58:50 -0400

    export uses the album.fms, "z sides" - does not compile yet
    Cherry-pick comment: DOES NOT EVEN EXPORT THE MUSIC
    (cherry picked from commit dfd7b2f9e5bb18eaee14f7fb6d6eaa5a0153f0ae)

---
+++ LEVELS/include/lvlset_Z/menutext.h
@@ -0,0 +1,23 @@
+
+// Exported by export_levels.py
+
+const char levelText00[12] = "YOU SHOULDNT";
+const char levelText01[ 7] = "BE HERE";
+
+
+const char* const levelTextsUpper[] = {
+	levelText00,
+};
+
+const uint8_t levelTextsUpperSize[] = {
+	sizeof(levelText00),
+};
+
+
+const char* const levelTextsLower[] = {
+	levelText01,
+};
+
+const uint8_t levelTextsLowerSize[] = {
+	sizeof(levelText01),
+};
