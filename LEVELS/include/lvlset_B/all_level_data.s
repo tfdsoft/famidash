987b494393ce5001d8852d6afbc1b3540d409a04
kandowontu <95717282+kandowontu@users.noreply.github.com>
2025-06-10 15:40:28 -0400

    B: smol fofiifofiifofii fix

--- LEVELS/include/lvlset_B/all_level_data.s
+++ LEVELS/include/lvlset_B/all_level_data.s
@@ -338,7 +338,7 @@
 		.incbin "EXPORTS/highlife.lz.1.bin" ; Size: 2540
 
 
-.segment "DAT_BANK_12"	; Total bank size: 8188 bytes
+.segment "DAT_BANK_12"	; Total bank size: 8096 bytes
 	.export level_data_fofiifofiifofii
 	level_data_fofiifofiifofii:
 	; Header
@@ -354,24 +354,7 @@
 		.byte $0F ;______________ Starting ground color
 		.byte 47 ;_______________ Level height
 	; Level data
-		.incbin "EXPORTS/fofiifofiifofii.lz.bin" ; Size: 5449
-
-	.export level_data_test4
-	level_data_test4:
-	; Header
-		.byte song_sonic_blaster ;___ Song ID
-		.byte 0 ;____________________ Starting game mode
-		.byte 0 ;____________________ Starting speed
-		.byte 0 ;____________________ Disable parallax
-		.byte _DECO1 ;_______________ Deco type
-		.byte _SPIKESA ;_____________ Spike set
-		.byte _BLOCKSA ;_____________ Block set
-		.byte _SAWBLADESA ;__________ Sawblade set
-		.byte $11 ;__________________ Starting background color
-		.byte $11 ;__________________ Starting ground color
-		.byte 57 ;___________________ Level height
-	; Level data
-		.incbin "EXPORTS/test4.lz.bin" ; Size: 88
+		.incbin "EXPORTS/fofiifofiifofii.lz.bin" ; Size: 5445
 
 
 .segment "DAT_BANK_13"	; Total bank size: 8171 bytes
@@ -412,7 +395,7 @@
 		.incbin "EXPORTS/oceane.lz.bin" ; Size: 2927
 
 
-.segment "DAT_BANK_15"	; Total bank size: 8098 bytes
+.segment "DAT_BANK_15"	; Total bank size: 8186 bytes
 	.export level_data_generationretro
 	level_data_generationretro:
 	; Header
@@ -429,6 +412,23 @@
 		.byte 27 ;____________________ Level height
 	; Level data
 		.incbin "EXPORTS/generationretro.lz.bin" ; Size: 5202
+
+	.export level_data_test4
+	level_data_test4:
+	; Header
+		.byte song_sonic_blaster ;___ Song ID
+		.byte 0 ;____________________ Starting game mode
+		.byte 0 ;____________________ Starting speed
+		.byte 0 ;____________________ Disable parallax
+		.byte _DECO1 ;_______________ Deco type
+		.byte _SPIKESA ;_____________ Spike set
+		.byte _BLOCKSA ;_____________ Block set
+		.byte _SAWBLADESA ;__________ Sawblade set
+		.byte $11 ;__________________ Starting background color
+		.byte $11 ;__________________ Starting ground color
+		.byte 57 ;___________________ Level height
+	; Level data
+		.incbin "EXPORTS/test4.lz.bin" ; Size: 88
 
 
 .segment "DAT_BANK_16"	; Total bank size: 7965 bytes
