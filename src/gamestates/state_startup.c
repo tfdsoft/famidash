941663350e3c9cab438322fbaef9ed5e5e7241c8
UserSniper <vruh.69@gmail.com>
2025-10-14 21:30:52 -0400

    warning screen

--- src/gamestates/state_startup.c
+++ src/gamestates/state_startup.c
@@ -1,4 +1,7 @@
 void state_startup() {
+    music_play(0);
+    famistudio_music_stop();
+
     ppu_off();
 
     set_prg_8000(32);
@@ -6,23 +9,19 @@ void state_startup() {
     vram_copy(chr_font,0x600);
     vram_copy(chr_logo,0x600);
 
+    set_prg_8000(nametable_bank_0);
     vram_adr(0x2000);
     vram_unrle(nt_logo);
 
     ppu_on_all();
     pal_fade_to(0,4);
 
-    music_play(0);
-    famistudio_music_stop();
 
-    sfx_play(1,0);
-    
-    //set_prg_a000(36); 
-    //famistudio_init(1,0xa000);
-    //famistudio_music_play(0);
     for(char stall=8; stall>0; stall--){
         ppu_wait_nmi();
     }
+
+    sfx_play(1,0);
 
     for(char stall=90; stall>0; stall--){
         ppu_wait_nmi();
@@ -33,7 +32,20 @@ void state_startup() {
         if((stall >= 80) && (stall < 83)) pal_bright(2);
     }
 
-    music_play(song);
+    pal_fade_to(4,0);
+    ppu_off();
+
+    set_prg_8000(nametable_bank_0);
+    vram_adr(0x2000);
+    vram_unrle(nt_warning);
+
+    pal_col(0,0x11);
+    pal_col(1,0x0f);
+
+    ppu_on_all();
+    pal_fade_to(0,4);
+
+    music_play(1);
 
     while(1){
         ppu_wait_nmi();
