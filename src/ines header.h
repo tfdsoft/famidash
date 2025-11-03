28dca55a5cd4c0ed98f7ddad6efa8772178db9dc
UserSniper <vruh.69@gmail.com>
2025-11-02 22:47:24 -0500

    increase chr-ram to 64k

--- src/ines header.h
+++ src/ines header.h
@@ -4,10 +4,9 @@
 // maxed out mmc3 prg
 MAPPER_PRG_ROM_KB(512);
 
-// 8kB of chr-ram
-// Will be increased to 256k with parallax implementation
+// 64kb of chr-ram should be fine
 MAPPER_CHR_ROM_KB(0);
-MAPPER_CHR_RAM_KB(16);
+MAPPER_CHR_RAM_KB(64);
 
 // four screens of characters
 MAPPER_USE_4_SCREEN_NAMETABLE;
