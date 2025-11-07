a4e9ae2deec3018fcdffff01b4ab3bb686b52614
kandowontu <95717282+kandowontu@users.noreply.github.com>
2025-11-06 19:25:08 -0500

    Really fix all retro mode death stuff

--- SAUCE/functions/reset_level.h
+++ SAUCE/functions/reset_level.h
@@ -22,11 +22,11 @@ void death_animation() {
 			dual == 1 ? oam_clear_two_players() : oam_clear_player();
 			
 			if (robotjumpframe[0] < 20) {
-				if (retro_mode && (gamemode == GAMEMODE_ROBOT || gamemode == GAMEMODE_UFO)) mmc3_set_2kb_chr_bank_0(iconbank1);
+				if (retro_mode && !(gamemode == GAMEMODE_ROBOT || gamemode == GAMEMODE_NINJA || gamemode == GAMEMODE_UFO || gamemode == GAMEMODE_SHIP)) mmc3_set_2kb_chr_bank_0(20);	//default set for explosion
 				oam_meta_spr(
 					idx16_load_hi_NOC(player_x, tmp2)-2,
 					idx16_load_hi_NOC(player_y, tmp2)-2,
-					(retro_mode && (gamemode == GAMEMODE_ROBOT || gamemode == GAMEMODE_UFO)) ? ExplodeR_Sprites[robotjumpframe[0] & 0x7F] :
+					(retro_mode && (gamemode == GAMEMODE_ROBOT || gamemode == GAMEMODE_NINJA || gamemode == GAMEMODE_UFO || gamemode == GAMEMODE_SHIP)) ? ExplodeR_Sprites[robotjumpframe[0] & 0x7F] :
 					Explode_Sprites[robotjumpframe[0] & 0x7F]);
 				++robotjumpframe[0];
 			}
