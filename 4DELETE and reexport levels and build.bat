4eb4d32bd4efdf9d69e4e16a0dd996511b5334c3
kandowontu <95717282+kandowontu@users.noreply.github.com>
2025-11-17 16:46:17 -0500

    Rebuild D sides - rerelease D sides

--- 4DELETE and reexport levels and build.bat
+++ 4DELETE and reexport levels and build.bat
@@ -3,6 +3,7 @@ del /Q TMP\*.*
 rmdir /s /q LEVELS\include\lvlset_A\exports
 rmdir /s /q LEVELS\include\lvlset_B\exports
 rmdir /s /q LEVELS\include\lvlset_C\exports
+rmdir /s /q LEVELS\include\lvlset_D\exports
 rmdir /s /q LEVELS\include\lvlset_HUGE\exports
 call LEVELS\export_levels.bat
 call build.bat
