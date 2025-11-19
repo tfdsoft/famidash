4231d75a572ece13ab6f4dcae38756b752fe30cb
kandowontu <95717282+kandowontu@users.noreply.github.com>
2025-11-19 09:52:05 -0500

    Tile 0 no longer needed in levels

--- LEVELS/export_levels.py
+++ LEVELS/export_levels.py
@@ -130,7 +130,11 @@ def getCsvDataFromTmx(fileHandle, layerNames) -> list:
 	lines = list(csv.reader(csvData[0].text.strip().splitlines()))
 
 	# get local tileset IDs
-	lookup = {0: -1}
+	# For tile layers, empty tile (0) should become 1 instead of -1
+	if 'SP' in layerNames:
+		lookup = {0: -1}   # sprite layer
+	else:
+		lookup = {0: 0}    # tile layer
 	for line in lines:
 		idx = 0
 		while idx < len(line):
