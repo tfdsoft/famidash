e562fb1fdfa22d1501c4911c00cd66d6936285ff
kandowontu <95717282+kandowontu@users.noreply.github.com>
2025-11-19 00:47:03 -0500

    Add a default ground/bg to every level

--- LEVELS/LEVEL DATA/lvlset_D/famidash - Copy.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="famidash - Copy" tilewidth="16" tileheight="16" tilecount="256" columns="16">
- <image source="../../../GRAPHICS/famidash - Copy.bmp" width="256" height="256"/>
-</tileset>
