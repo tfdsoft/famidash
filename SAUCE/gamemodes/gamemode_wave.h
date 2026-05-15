eb01ac0d0d4f4c261d4f122f11f16715c5562e02
kandowontu <kandowontu@me.com>
2026-05-15 12:04:22 -0400

    Update gamemode_wave.h

--- SAUCE/gamemodes/gamemode_wave.h
+++ SAUCE/gamemodes/gamemode_wave.h
@@ -38,7 +38,7 @@ void wave_movement(){
 	Generic.x = high_byte(currplayer_x) + 4;
 	
 	// this literally offsets the collision down 2 pixel for the vel reset to happen every frame instead of each other frame
-	Generic.y = high_byte(currplayer_y) + (currplayer_mini ? 1 : 4);
+	Generic.y = high_byte(currplayer_y) + (currplayer_mini ? 0 : 4);
 	
 	
 
