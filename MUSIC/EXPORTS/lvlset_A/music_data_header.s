aad7e43b09b795561eae72a8abca3e1669c53363
ADM228 <91823517+ADM228@users.noreply.github.com>
2026-06-30 18:38:29 +0300

    musexpt: acquire music data and dmc bank sizes

--- MUSIC/EXPORTS/lvlset_A/music_data_header.s
+++ MUSIC/EXPORTS/lvlset_A/music_data_header.s
@@ -3,40 +3,40 @@
 ; Music data banks
 .segment "DAT_BANK_24"
 	firstMusicBankPtr := *
-	.include "music_0.s"
+	.include "music_0.s"	; Approx. size: 7305 bytes
 .segment "DAT_BANK_25"
-	.include "music_1.s"
+	.include "music_1.s"	; Approx. size: 7286 bytes
 .segment "DAT_BANK_26"
-	.include "music_2.s"
+	.include "music_2.s"	; Approx. size: 7309 bytes
 .segment "DAT_BANK_27"
-	.include "music_3.s"
+	.include "music_3.s"	; Approx. size: 7212 bytes
 .segment "DAT_BANK_28"
-	.include "music_4.s"
+	.include "music_4.s"	; Approx. size: 7164 bytes
 .segment "DAT_BANK_29"
-	.include "music_5.s"
+	.include "music_5.s"	; Approx. size: 6886 bytes
 .segment "DAT_BANK_2A"
-	.include "music_6.s"
+	.include "music_6.s"	; Approx. size: 7377 bytes
 .segment "DAT_BANK_2B"
-	.include "music_7.s"
+	.include "music_7.s"	; Approx. size: 7713 bytes
 .segment "DAT_BANK_2C"
-	.include "music_8.s"
+	.include "music_8.s"	; Approx. size: 7654 bytes
 .segment "DAT_BANK_2D"
-	.include "music_9.s"
+	.include "music_9.s"	; Approx. size: 7416 bytes
 .segment "DAT_BANK_2E"
-	.include "music_10.s"
+	.include "music_10.s"	; Approx. size: 7635 bytes
 .segment "DAT_BANK_2F"
-	.include "music_11.s"
+	.include "music_11.s"	; Approx. size: 7527 bytes
 .segment "DAT_BANK_30"
-	.include "music_12.s"
+	.include "music_12.s"	; Approx. size: 4652 bytes
 
 ; DMC banks
 .segment "DAT_BANK_31"
 	firstDMCBankPtr := *
-	.incbin "music_bank0.dmc"
+	.incbin "music_bank0.dmc"	; Size: 7552 bytes
 .segment "DAT_BANK_32"
-	.incbin "music_bank1.dmc"
+	.incbin "music_bank1.dmc"	; Size: 8064 bytes
 .segment "DAT_BANK_33"
-	.incbin "music_bank2.dmc"
+	.incbin "music_bank2.dmc"	; Size: 8064 bytes
 
 ; PCM includes
 .segment "PCM_BANK"
