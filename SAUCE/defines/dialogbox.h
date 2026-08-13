499c79a8ca5425b3fc659276d2d6f539111d8054
dajinkosa <91823517+dajinkosa@users.noreply.github.com>
2026-08-14 00:00:38 +0300

    dialogbox: comment out unused dialog boxes

--- SAUCE/defines/dialogbox.h
+++ SAUCE/defines/dialogbox.h
@@ -38,13 +38,13 @@ void draw_dialog_box(const char * data);
 
 const char dialogBox_saveFileSafetyHeader[] = "$$SAVE$$FILE$$SAFETY$$\v\2";
 const char dialogBox_pleasePressB[] = "\a please hold  (RESET)\v\2  while powering off\v\2\t\3real nes/famicom \v\2  hardware  to avoid\v\2 save file corruption\v\2\b  [\t\20]  \n  $PRESS$ANY$BUTTON$\n  $$$TO$$CONTINUE$$$\n\b  {\t\20}  ";
-const char dialogBox_itIsNowSafe[] = "\a\n  it is now safe to\v\2 turn off your system\v\6\b  [\t\20]  \n  $$$$PRESS$B$TO$$$$\n  $$$$GO$BACK$TO$$$$\n  $THE$TITLE$SCREEN$\n\b  {\t\20}  ";
+// const char dialogBox_itIsNowSafe[] = "\a\n  it is now safe to\v\2 turn off your system\v\6\b  [\t\20]  \n  $$$$PRESS$B$TO$$$$\n  $$$$GO$BACK$TO$$$$\n  $THE$TITLE$SCREEN$\n\b  {\t\20}  ";
 
-const char dialogBox_wrongSaveFileVersion[] = " for a\a version";
-const char dialogBox_nolder[] = "n older";
-const char dialogBox_newer[] = " newer ";
-const char dialogBox_saveFileMissingCorrupt[] = "  missing or corrupt";
-const char dialogBox_saveIssues[] = "$$INVALID$$SAVE$FILE$$\v\5  your save  file is\v\2\a\v\6 (A;$CREATE$NEW$SAVE)\v\2 (B;$LOAD$ANYWAY$$$$)";
+// const char dialogBox_wrongSaveFileVersion[] = " for a\a version";
+// const char dialogBox_nolder[] = "n older";
+// const char dialogBox_newer[] = " newer ";
+// const char dialogBox_saveFileMissingCorrupt[] = "  missing or corrupt";
+// const char dialogBox_saveIssues[] = "$$INVALID$$SAVE$FILE$$\v\5  your save  file is\v\2\a\v\6 (A;$CREATE$NEW$SAVE)\v\2 (B;$LOAD$ANYWAY$$$$)";
 
 #include "defines/charmap/no_remap_charmap.h"
 #pragma rodata-name (pop)
